f9337a69eaa27620e3f70127cc60232cc9754475
illinis <leoreslopes@gmail.com>
2024-10-07 04:57:52 -0300

    feat(logger): add init_logger function to initial_setup function

--- src/timer.c
+++ src/timer.c
@@ -1,15 +1,31 @@
 #include "../include/timer.h"
 #include "../include/utils.h"
+#include "../include/logger.h"
 
 int time_remaining = 3600;
 int break_time = 1800;
+int action_timer = 0;
+
 gboolean running = FALSE;
 gint timer_id = 0;
 gboolean on_break = FALSE;
 
 void initial_setup(int argc, char* argv[]){
+  LoggerData logger_data;
+  
   parse_time_remaining_countdown(argc, argv, &time_remaining);
   parse_break_time_countdown(argc, argv, &break_time);
+
+  char time_str[16];
+  snprintf(time_str, sizeof(time_str), "%02d:%02d", time_remaining / 60, time_remaining % 60);
+  logger_data.timer = time_str;
+  
+  char break_time_str[16];
+  snprintf(break_time_str, sizeof(break_time_str), "%02d:%02d", break_time / 60, break_time % 60);
+  logger_data.break_timer = break_time_str;
+
+  init_logger();
+  output_header(logger_data);
 }
 
 void deactivate_on_break(GtkWidget *widget, gpointer user_data){
@@ -32,10 +48,12 @@ gboolean update_timer(gpointer user_data) {
 
   if (time_remaining > 0 && !on_break) {
     time_remaining--;
+    action_timer++;
 
     if(break_time > 1) break_time--;
     else {
       on_break = TRUE;
+      
       
       GdkDisplay *display = gdk_display_get_default();
       if (display != NULL) {
